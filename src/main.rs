b1d64e9457bce5cef66745eca795b347c6df568a
Spencer Elliott <spencer@sharpthinking.ca>
2019-12-20 15:50:26 -0500

    Added ability to set individual pixels

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,33 @@ use winit::event_loop::{EventLoop, ControlFlow};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+use std::time::Instant;
+
+const SURFACE_WIDTH: u32 = 320;
+const SURFACE_HEIGHT: u32 = 240;
+
+fn clear(screen: &mut [u8]) {
+    for (i, byte) in screen.iter_mut().enumerate() {
+        *byte = if i % 4 == 3 { 255 } else { 0 };
+    }
+}
+
+fn set_pixel(frame: &mut [u8], x: u32, y: u32, rgba: [u8; 4]) {
+    let index = (((y * SURFACE_WIDTH) + x) * 4) as usize;
+    let pixel_slice = &mut frame[index..index+4];
+
+    pixel_slice[0] = rgba[0];
+    pixel_slice[1] = rgba[1];
+    pixel_slice[2] = rgba[2];
+    pixel_slice[3] = rgba[3];
+}
 
 fn main() -> Result<(), Error> {
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
 
     let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let size = LogicalSize::new(1024 as f64, 768 as f64);
         WindowBuilder::new()
             .with_title("Hello tinyrenderer")
             .with_inner_size(size)
@@ -27,9 +45,11 @@ fn main() -> Result<(), Error> {
 
     let mut pixels = {
         let surface = Surface::create(&window);
-        let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, surface);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+        let surface_texture = SurfaceTexture::new(SURFACE_WIDTH, SURFACE_HEIGHT, surface);
+        Pixels::new(SURFACE_WIDTH, SURFACE_HEIGHT, surface_texture)?
     };
+
+    let mut last_frame = Instant::now();
 
     event_loop.run(move |event, _, control_flow| {
         if let Event::WindowEvent {
@@ -37,7 +57,22 @@ fn main() -> Result<(), Error> {
             ..
         } = event
         {
+            let previous_frame_time = last_frame;
+
+            let frame = pixels.get_frame();
+            clear(frame);
+            set_pixel(frame, 10, 10, [255, 0, 0, 0]);
+            set_pixel(frame, 11, 10, [255, 0, 0, 0]);
+            set_pixel(frame, 12, 10, [255, 0, 0, 0]);
             pixels.render();
+
+            last_frame = Instant::now();
+
+            let delta = last_frame - previous_frame_time;
+
+            let fps = (1.0 / ((delta.as_millis() as f64) / 1000.0)).round();
+
+            window.set_title(&format!("tinyrenderer ({} fps)", fps));
         }
 
         if input.update(event) {
@@ -48,14 +83,6 @@ fn main() -> Result<(), Error> {
 
             if let Some(factor) = input.hidpi_changed() {
                 hidpi_factor = factor;
-            }
-
-            if let Some(size) = input.window_resized() {
-               let size = size.to_physical(hidpi_factor);
-               let width = size.width.round() as u32;
-               let height = size.height.round() as u32;
-
-               pixels.resize(width, height);
             }
 
             window.request_redraw();
